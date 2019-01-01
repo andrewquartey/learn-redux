5c47114e6acbb4372e386663ad469c76b5f92330
Andrew Quartey <yestrup@gmail.com>
2019-01-01 18:53:25 +0000

    created Single and Photogrid components

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from  'react-router'
+
+const Photo = React.createClass({
+    render(){
+        return (
+            <div className="single-photo">
+                single
+            </div>
+        )
+    }
+})
+
+export default Photo;
